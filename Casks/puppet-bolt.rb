f3c8e254902302502a0776a2af8bfa495ec87efa
Michael Smith <michael.smith@puppet.com>
2018-06-07 14:30:42 -0700

    (BOLT-572) Correct Bolt SHAs for package signing
    Previous SHAs were calculated on pre-released packages, which hadn't yet
    been signed.

--- Casks/puppet-bolt.rb
+++ Casks/puppet-bolt.rb
@@ -2,11 +2,11 @@ cask 'puppet-bolt' do
   version '0.20.6'
 
   if MacOS.version == :el_capitan
-    sha256 'b628650adcdcf01e67a2e469e53422ba8df39d763faa60302165b08f07ce6687'
+    sha256 '70656920c0d03c7363df752f03ae00c7ca5d3bde941d8671cf3a264d397cd86c'
   elsif MacOS.version == :sierra
-    sha256 '83cbc6c3b40d5e19023adb294c8c8d5ab7c0b6ea735aa95411b6f1da1a9108ff'
+    sha256 '9451360b8820fbbb8de60e6ef66b0dbec7ae020bee4de5904d1c3abf863ad8c4'
   else
-    sha256 '5c8a1e13808ad623f5d2265312a40b1dedced806bb0500b887cd71929cd11fdd'
+    sha256 'bbe5761c7686c60e07c6c4769aa414e6f03ebd161fb014210bd66949f0d93254'
   end
 
   # downloads.puppetlabs.com/mac was verified as official when first introduced to the cask
