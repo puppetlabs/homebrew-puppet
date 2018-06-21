a0ddc54631b394ae46a7c733c253a1a30bb10c34
Bryan Jen <bryan.jen@gmail.com>
2018-06-21 14:17:37 -0700

    Add PDK 1.6.0.0 and packages for high sierra

--- Casks/pdk.rb
+++ Casks/pdk.rb
@@ -1,14 +1,35 @@
 cask 'pdk' do
-  version '1.5.0.0'
+  version '1.6.0.0'
 
-  # TODO: change this to a case/switch once we add 10.13
-  if MacOS.version <= :el_capitan
-    sha256 '4de3c31b27aa0201a3c0eeff413a19349c10541854687ac3874aed49238a7983'
-    url "https://puppet-pdk.s3.amazonaws.com/pdk/#{version}/repos/apple/10.11/PC1/x86_64/pdk-#{version}-1.osx10.11.dmg"
+  version_map = {
+    high_sierra: { 
+      sha: 'cd4e2719d83f0b14df3f284c2b903cccda8cc9ac7f0d8de53f8bfa26dc8e0427',
+      ver: '10.13'
+    },
+    sierra: {
+      sha: '4c9b64e8f228a8a42996b38b26091c053c5bbcbb3340fd4f78354c981f100f64',
+      ver: '10.12'
+    },
+    el_capitan: {
+      sha: '1b298bc1842318dc9b47792e5c2959487218e1a1f2cd94e855a3575b0a5aa139',
+      ver: '10.11'
+    }
+  }
+
+  case MacOS.version
+  when :high_sierra
+    sha = version_map[:high_sierra][:sha]
+    os_ver = version_map[:high_sierra][:ver]
+  when :sierra
+    sha = version_map[:sierra][:sha]
+    os_ver = version_map[:sierra][:ver]
   else
-    sha256 '713668b5113cf2d9309cfd199be0d9a52357d1c878c2aa8ef62ebb1ace55649b'
-    url "https://puppet-pdk.s3.amazonaws.com/pdk/#{version}/repos/apple/10.12/PC1/x86_64/pdk-#{version}-1.osx10.12.dmg"
+    sha = version_map[:el_capitan][:sha]
+    os_ver = version_map[:el_capitan][:ver]
   end
+
+  sha256 sha
+  url "https://puppet-pdk.s3.amazonaws.com/pdk/#{version}/repos/apple/#{os_ver}/PC1/x86_64/pdk-#{version}-1.osx#{os_ver}.dmg"
 
   name 'Puppet Development Kit'
   homepage 'https://github.com/puppetlabs/pdk'
