b871c97558ab451dbecd18edc68201f821999444
Michael Smith <michael.smith@puppet.com>
2018-08-08 17:41:57 -0700

    (maint) Update cask generation to support puppet-agent
    Updates the Rake task to generate casks to support differing versions
    for puppet-agent. Add a new puppet-agent template. Regenerate all casks.

--- Casks/pdk.rb
+++ Casks/pdk.rb
@@ -1,22 +1,22 @@
 cask 'pdk' do
-  version '1.7.0.0'
-
-  case MacOS.version.to_sym
-  when :el_capitan
+  case MacOS.version
+  when '10.11'
+    version '1.7.0.0'
     sha256 '451fc9fda86bd8df0a1be72c6145f433828075e304750df125b47e4c30a3567e'
-  when :sierra
+  when '10.12'
+    version '1.7.0.0'
     sha256 'ae81c8fe3009d1e74fca4c81e8d635f86207dd8c40f2af057b7544c825e8b8b2'
-  when :high_sierra
+  when '10.13'
+    version '1.7.0.0'
     sha256 '484d8a317db2074d10c0c89818bbf712412e2bb493ae6464a6575990f114211b'
   end
 
+  depends_on macos: '>= 10.11'
   url "https://downloads.puppet.com/mac/puppet5/#{MacOS.version}/x86_64/pdk-#{version}-1.osx#{MacOS.version}.dmg"
+  pkg "pdk-#{version}-1-installer.pkg"
+
   name 'Puppet Development Kit'
   homepage 'https://github.com/puppetlabs/pdk'
-
-  depends_on macos: '>= 10.11'
-
-  pkg "pdk-#{version}-1-installer.pkg"
 
   uninstall pkgutil: 'com.puppetlabs.pdk'
 end
--- Casks/puppet-bolt.rb
+++ Casks/puppet-bolt.rb
@@ -1,22 +1,22 @@
 cask 'puppet-bolt' do
-  version '0.21.6'
-
-  case MacOS.version.to_sym
-  when :el_capitan
+  case MacOS.version
+  when '10.11'
+    version '0.21.6'
     sha256 'ea2a4cfed3e2b5c35020788506fd195166083aeef0d9d45a8f3522c9cd129727'
-  when :sierra
+  when '10.12'
+    version '0.21.6'
     sha256 '20c9a2abc3ddfcbc8d94681222cad7814fe4881bff51d571e3ce256aecbdcb14'
-  when :high_sierra
+  when '10.13'
+    version '0.21.6'
     sha256 '7bd2f8e73b4526616cdc6f87fc355dc1cb2ae6a79075e86a2c83d08040b1b3f9'
   end
 
+  depends_on macos: '>= 10.11'
   url "https://downloads.puppet.com/mac/puppet5/#{MacOS.version}/x86_64/puppet-bolt-#{version}-1.osx#{MacOS.version}.dmg"
+  pkg "puppet-bolt-#{version}-1-installer.pkg"
+
   name 'Puppet Bolt'
   homepage 'https://github.com/puppetlabs/bolt'
-
-  depends_on macos: '>= 10.11'
-
-  pkg "puppet-bolt-#{version}-1-installer.pkg"
 
   uninstall pkgutil: 'com.puppetlabs.puppet-bolt'
 end
