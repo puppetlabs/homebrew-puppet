71ec143a176f7bb6bdbae922bf525148bd77fc4b
Bryan Jen <bryan.jen@gmail.com>
2019-01-29 17:59:17 -0700

    (PDK-1262) Update pdk to 1.9.0.0

--- Casks/pdk.rb
+++ Casks/pdk.rb
@@ -2,16 +2,20 @@ cask 'pdk' do
   case MacOS.version
   when '10.11'
     os_ver = '10.11'
-    version '1.8.0.0'
-    sha256 'a5c404e7a33ca2f47aa575958a9f0f879ef5f5ac909e48fe2b6b0cfe69ff4fa7'
+    version '1.9.0.0'
+    sha256 '997ad11c3e6b0c300ff2a0b822db2e93eea28f0a19036b404f4b14451c84cb9c'
   when '10.12'
     os_ver = '10.12'
-    version '1.8.0.0'
-    sha256 '26548072664f6126975ba205e5334c28a05da68a50bd1534b4785c7293e196bc'
-  else
+    version '1.9.0.0'
+    sha256 '9da5599e376b33a4086d52f38e49ab1eb8c746cb4b6fe313251b303d2a3aca1e'
+  when '10.13'
     os_ver = '10.13'
-    version '1.8.0.0'
-    sha256 'f0418c82df0745c0968def1ec2639610777c7492bc979a4fd34c0a3dc33ecda1'
+    version '1.9.0.0'
+    sha256 '776fc996b55ee6e829ac88c48e3104b540f92d140d7c272a634104a896ef9db1'
+  else
+    os_ver = '10.14'
+    version '1.9.0.0'
+    sha256 '509159873142378b282f8e6c1d645eb37480ecdc811747303057dbee77a95afc'
   end
 
   depends_on macos: '>= 10.11'
