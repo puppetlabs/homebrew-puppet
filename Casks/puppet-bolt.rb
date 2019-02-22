38ee3143979abc6eb6a8fc27933aa133e8ba6825
donoghuc <cas.donoghue@gmail.com>
2019-02-21 17:05:36 -0800

    (BOLT-1138) Update puppet-bolt to 1.12.0

--- Casks/puppet-bolt.rb
+++ Casks/puppet-bolt.rb
@@ -2,20 +2,20 @@ cask 'puppet-bolt' do
   case MacOS.version
   when '10.11'
     os_ver = '10.11'
-    version '1.11.0'
-    sha256 'c0190edd1c44ac4413100063c521482d54b746d938d0ab74a3c94afb3a95406d'
+    version '1.12.0'
+    sha256 '51b97820dd13ac76afea530f79ba2fce255ca7a04aaebb6021625e236886eb94'
   when '10.12'
     os_ver = '10.12'
-    version '1.11.0'
-    sha256 '7509c224d704e3665ac3c31b9da4054338e5a59466e9e2d936a32aa9439eb803'
+    version '1.12.0'
+    sha256 '2286ee1e5d05816a132ee1dd0a7a930d1f173010e722fa5796c34e4e03cde0c7'
   when '10.13'
     os_ver = '10.13'
-    version '1.11.0'
-    sha256 '123707b7389fbdbc2a6d70568289cb65f1f1ca99771bdb317cef97dd71c4c318'
+    version '1.12.0'
+    sha256 'e391ff71b22125fb9791623d96ab4d441829a8dec27056b36c092f94b78339ed'
   else
     os_ver = '10.14'
-    version '1.11.0'
-    sha256 '068e192372e5d98f191dff56af306034c28bade4ce4daeff9e371a40c608983e'
+    version '1.12.0'
+    sha256 '797ee05fbe91dc1c1b0abbb843eb6120d6dfd17552cf9afe1275dc7fb8550037'
   end
 
   depends_on macos: '>= 10.11'
