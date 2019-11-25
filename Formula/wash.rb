f26a5233104d55be7bdccbc79dc72c8061f93419
ekinanp <ekinanp@users.noreply.github.com>
2019-11-25 22:39:03 +0000

    Update Wash to 0.14.0

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,8 +1,8 @@
 class Wash < Formula
-  version = "0.13.1"
+  version = "0.14.0"
   homepage "https://puppetlabs.github.io/wash"
   url "https://github.com/puppetlabs/wash/archive/#{version}.tar.gz"
-  sha256 "34d0e3566c313024da393276965d0364761fff305e02c2de0bd182b82cf679a8"
+  sha256 "9170818c7798ae88e0e2e920cb2b59a6a8520634d4d3feab2b44050f6dbb63a9"
 
   head "https://github.com/puppetlabs/wash.git"
 
