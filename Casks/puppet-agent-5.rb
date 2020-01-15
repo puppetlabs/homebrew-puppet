4ec23833d15e0304bcd53c9a7ccc71fc67815579
Dorin Pleava <dorin.pleava@puppet.com>
2020-01-15 12:51:07 +0200

    (PA-3038) Update homebrew versions for PA 5.5.8

--- Casks/puppet-agent-5.rb
+++ Casks/puppet-agent-5.rb
@@ -10,16 +10,16 @@ cask 'puppet-agent-5' do
     sha256 'fe60c24d2b964f161599bf4594c9e871f161707375b81c6b1e998e8cfce13058'
   when '10.12'
     os_ver = '10.12'
-    version '5.5.17'
-    sha256 '187998fa66b20b296b1e9fae342534b9e18e38f1eefa3f7d9e2fe1dca0644055'
+    version '5.5.18'
+    sha256 '0f701c542a7c234f86b1b92d07ae1272716ae1be339e8505546be5eebcdcb4fb'
   when '10.13'
     os_ver = '10.13'
-    version '5.5.17'
-    sha256 'b50680b1d4d5f4ffef03d50bcd33cde73007657086bdca7060fb07ce0d3e6e2a'
+    version '5.5.18'
+    sha256 'be76193cd6298e372667fc65381a7f711c0585a2f70c355014dc4d59ae8948f6'
   else
     os_ver = '10.14'
-    version '5.5.17'
-    sha256 'b6e5cd9857e01a0044ffd1f8332da792c85b0b4e03e1336c1d5aeb5bfae1f592'
+    version '5.5.18'
+    sha256 '06bfdf742356edbd4532246134a3bae43e36faa4daacc94017da760fdb3f9867'
   end
 
   depends_on macos: '>= :yosemite'
