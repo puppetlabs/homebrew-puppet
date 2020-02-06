5dae4b17bcd619cf92f9436362e16fc8c5028295
Tim Sharpe <tim@bombasticmonkey.com>
2020-02-06 13:34:32 +1100

    (PDK-1606) Update PDK cask to 1.16.0.1

--- Casks/pdk.rb
+++ Casks/pdk.rb
@@ -2,20 +2,20 @@ cask 'pdk' do
   case MacOS.version
   when '10.11'
     os_ver = '10.11'
-    version '1.15.0.0'
-    sha256 'df270ba66de5b37c64ee970aec06cfd08f105984b978c965c99e3634db053311'
+    version '1.16.0.1'
+    sha256 'a50da11a63c140e2e0e60c90af6352de724705da1afb1e285315fe401d93889b'
   when '10.12'
     os_ver = '10.12'
-    version '1.15.0.0'
-    sha256 'de717de5f74a6e6127b5d8c349fff15cf4b8e680ad697e68a58173af227fdcd6'
+    version '1.16.0.1'
+    sha256 '92eb42d7e7dd45e85c4d9a3a2b8b107a3eb9459174d7300af9f67d63d5babb44'
   when '10.13'
     os_ver = '10.13'
-    version '1.15.0.0'
-    sha256 'ac60b2fe2588944e8108538830d3a88cd7507db188ac14aeb2051a8cb6bf11b9'
+    version '1.16.0.1'
+    sha256 '47adc57a31ee275cb9beb31aa39a09ee6adde6425dd9669445861c33fece312d'
   else
     os_ver = '10.14'
-    version '1.15.0.0'
-    sha256 '2b41461a0838367c8da30588055d2464ac3001dd606349dd2af4b32f78db1023'
+    version '1.16.0.1'
+    sha256 '88891e466db05273f2c961dd0df00adc2b4fded0f754946859271fa2d947b9be'
   end
 
   depends_on macos: '>= :el_capitan'
