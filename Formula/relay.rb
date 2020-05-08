34b04366ff3e685f231287fbefbe393398cb0817
Relay Autobot <relay@users.noreply.github.com>
2020-05-08 22:10:07 +0000

    Update Relay to tagged version v4.0.4

--- Formula/relay.rb
+++ Formula/relay.rb
@@ -1,9 +1,9 @@
 class Relay < Formula
-  version "v3.4.0"
+  version "v4.0.4"
   homepage "https://relay.sh/"
   url "https://github.com/puppetlabs/relay/releases/download/#{version}/nebula-#{version}-darwin-amd64", 
     :using => :nounzip
-  sha256 "2b43e3deb12c6ad2449abcb85ca095cbefd8d6c35c98b3987986d43c7b375916"
+  sha256 "758d53d68aa161f0f7cfd075a38413d047c47e6e0c40269a096b139bee7789f1"
 
   def install
     bin.install "nebula-#{version}-darwin-amd64" => "relay"
