b781f3f65c2067d364ecba66e0a1956f22f5e5f4
Ciprian Badescu <ciprian.badescu@puppet.com>
2021-02-17 09:40:40 +0200

    (maint) publich client tools 21.0.0

--- Casks/pe-client-tools.rb
+++ Casks/pe-client-tools.rb
@@ -1,17 +1,17 @@
 cask 'pe-client-tools' do
   case MacOS.version
-  when '10.12'
-    os_ver = '10.12'
-    version '19.3.0'
-    sha256 'd784e74325079ebecdbbc72ab5214c7f54be8ae1a38a6d4954bf52f7c02b1142'
+  when '10.14'
+    os_ver = '10.14'
+    version '21.0.0'
+    sha256 'ca433c154a329c5fb57d46459f7086974bfe9d9b1966b9961a44ac739b7d09d9'
   else
-    os_ver = '10.13'
-    version '19.3.0'
-    sha256 'bccf0c588dd7d5e9dab92716e8471214c381e60f4f9246ba605091b253fb59f5'
+    os_ver = '10.15'
+    version '21.0.0'
+    sha256 '4c0b9bcb5a14159e93b82e02bc27c6f3ce80291f3b74fb0ce50f2dbf1727d7d4'
   end
 
-  depends_on macos: '>= :sierra'
-  url "https://pm.puppet.com/pe-client-tools/2019.3.0/#{version}/repos/apple/#{os_ver}/PC1/x86_64/pe-client-tools-#{version}-1.osx#{os_ver}.dmg"
+  depends_on macos: '>= :mojave'
+  url "https://pm.puppet.com/pe-client-tools/2021.0.0/#{version}/repos/apple/#{os_ver}/PC1/x86_64/pe-client-tools-#{version}-1.osx#{os_ver}.dmg"
   pkg "pe-client-tools-#{version}-1-installer.pkg"
 
   name 'PE Client Tools'
