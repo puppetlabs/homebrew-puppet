4e26f294253a3c4d94e2d2868fd97c3a6d0d8fa8
Ciprian Badescu <ciprian.badescu@puppet.com>
2021-02-17 09:39:50 +0200

    (maint) publish client tools 19.8.5

--- Casks/pe-client-tools-2019.8.rb
+++ Casks/pe-client-tools-2019.8.rb
@@ -1,20 +1,16 @@
 cask 'pe-client-tools-2019.8' do
   case MacOS.version
-  when '10.13'
-    os_ver = '10.13'
-    version '19.8.4'
-    sha256 '2c47f8ef4789d1cdf52d4b9d1e9d8e333101286117ab27bf6b9659929a7776fa'
   when '10.14'
     os_ver = '10.14'
-    version '19.8.4'
-    sha256 '1256319f55863af4ca8afb0eb3939e247a18104f5a0fd4cd4b59146487d99ca1'
+    version '19.8.5'
+    sha256 'dd4aa20af10f27f2c5fe58a8ca512dead51eb7bfa00b186d301c67135b108692'
   else
     os_ver = '10.15'
-    version '19.8.4'
-    sha256 '41b83875ff013d3e6dc91f59b49708d4076666e619d010c7cbce4f5412d0e3fa'
+    version '19.8.5'
+    sha256 '5be345ca3691f00b97c54f3cd5e523260257cb1e0589733dc1694b6241e5a7bb'
   end
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: '>= :mojave'
   url "https://pm.puppet.com/pe-client-tools/2019.8.0/#{version}/repos/apple/#{os_ver}/PC1/x86_64/pe-client-tools-#{version}-1.osx#{os_ver}.dmg"
   pkg "pe-client-tools-#{version}-1-installer.pkg"
 
