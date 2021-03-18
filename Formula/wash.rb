76a5cc52de9254631ff3ce71075640deca1c8c57
MikaelSmith <MikaelSmith@users.noreply.github.com>
2021-03-18 21:24:05 +0000

    Update Wash to 0.23.3

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,8 +1,8 @@
 class Wash < Formula
-  version = "0.23.2"
+  version = "0.23.3"
   homepage "https://puppetlabs.github.io/wash"
   url "https://github.com/puppetlabs/wash/archive/#{version}.tar.gz"
-  sha256 "b55233d2e0edf3dd7379f8fe66559646e7348224eb382480bc79a744cc812088"
+  sha256 "006258aa19f4f62c356db83b52f1858674991839eeb2bb3f36fc06494f9fa458"
 
   head "https://github.com/puppetlabs/wash.git"
 
