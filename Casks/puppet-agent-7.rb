399a4f0c06b1c7783812bf63c91b55f460c1263b
Gabriel Nagy <gabriel.nagy@puppet.com>
2021-06-02 10:18:38 +0300

    (PA-3728) Update puppet7 casks with 7.7.0 and add Big Sur

--- Casks/puppet-agent-7.rb
+++ Casks/puppet-agent-7.rb
@@ -2,12 +2,16 @@ cask 'puppet-agent-7' do
   case MacOS.version
   when '10.14'
     os_ver = '10.14'
-    version '7.6.1'
-    sha256 '0b455c44e417421c750205506c45c2d5c25689594727380bc2999f6376e1ee50'
-  else
+    version '7.7.0'
+    sha256 'f86ae90d5a0e2daea0222ae2507ffa25e27ff994424becb236bc2f20747b5985'
+  when '10.15'
     os_ver = '10.15'
-    version '7.6.1'
-    sha256 'f31b55a1a18424a53e8dc39b6f129760a0d4f098b4a063a22c1f6daea98fef60'
+    version '7.7.0'
+    sha256 '70fdf750c1542a1b68c48241e8d6c4322401714eb99f8c548eb2d7e3df92feeb'
+  else
+    os_ver = '11'
+    version '7.7.0'
+    sha256 '8dcfb8caaefcfdba1dc4f453a3c35261e579b54305d029f7de67d2862b3662f8'
   end
 
   depends_on macos: '>= :mojave'
