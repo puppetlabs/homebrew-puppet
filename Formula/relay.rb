b493db4c0b6f75791f4aed21a97181aba26ec43c
Relay Autobot <relay@users.noreply.github.com>
2021-07-20 15:34:21 +0000

    Update Relay to tagged version v4.4.2

--- Formula/relay.rb
+++ Formula/relay.rb
@@ -1,9 +1,9 @@
 class Relay < Formula
-  version "v4.3.5"
+  version "v4.4.2"
   homepage "https://relay.sh/"
   url "https://github.com/puppetlabs/relay/releases/download/#{version}/relay-#{version}-darwin-amd64", 
     :using => :nounzip
-  sha256 "8b3310d5d811071e6981e8d3e722cb5cfe07ab17e82853d785b5503b438162a9"
+  sha256 "88facbb232d1b29683317c97bd3fc55848b198a8b759bb3e73d00b4615636fa2"
 
   def install
     bin.install "relay-#{version}-darwin-amd64" => "relay"
