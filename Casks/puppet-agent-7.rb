7b12715fc2467de6695e6d0406e1174102f9fe36
Bobosila Victor <vbobosila1@gmail.com>
2021-07-21 11:44:54 +0300

    (PA-3852) Update homebrew-puppet for puppet-agent 7.9.0

--- Casks/puppet-agent-7.rb
+++ Casks/puppet-agent-7.rb
@@ -2,16 +2,16 @@ cask 'puppet-agent-7' do
   case MacOS.version
   when '10.14'
     os_ver = '10.14'
-    version '7.8.0'
-    sha256 'a7eaff09647fabef22be2eedcd1fedc184e551bb625d02a43b122c4de4d39a1e'
+    version '7.9.0'
+    sha256 '539f2cbacf20bd2bed3bd7f173dc07aa99d9305a8cb91408e450e8b39ba0177d'
   when '10.15'
     os_ver = '10.15'
-    version '7.8.0'
-    sha256 '0d068d542cd24aa0d18d8a40abce6c3ec7ff363dba5c8057018cadff9bbf8eee'
+    version '7.9.0'
+    sha256 '43fd14a21de0358555065cc86210c45df43ddcc5c27d083f4e11d9706c49d4c8'
   else
     os_ver = '11'
-    version '7.8.0'
-    sha256 '86b22b7c09e32eeba103e3c5f8c482bbe1629613316d05cf45ffeb6b76380f1e'
+    version '7.9.0'
+    sha256 '9686d89d73f2a7373bfb3360e98a6c0bdcf8d793a35d8b96192684ec07f83370'
   end
 
   depends_on macos: '>= :mojave'
