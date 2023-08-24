f046f7443187e2671b07e0c653ae0fa3b7387614
Michael Hashizume <michael.hashizume@puppet.com>
2023-08-24 10:33:06 -0700

    (PA-5689) Add support for puppet-agent 8.2.0

--- Casks/puppet-agent-8.rb
+++ Casks/puppet-agent-8.rb
@@ -8,19 +8,27 @@ cask 'puppet-agent-8' do
   case MacOS.version
   when '11'
     os_ver = '11'
-    version '8.1.0'
+    version '8.2.0'
     if arch == 'arm64'
       sha256 'nil'
     elsif arch == 'x86_64'
-      sha256 '83bce32591927318ace67664038ce40153b06aab5358a3f3452cd54508c4d5c5'
+      sha256 '82fea415c6104f93b55f78d0f778fbeca63e0f2e2fc360882eba8550d33bc833'
+    end
+  when '12'
+    os_ver = '12'
+    version '8.2.0'
+    if arch == 'arm64'
+      sha256 '4a6451f68f411ba7b3fa37d73e7b8c847b766bab6a0bc583effff33ca3b1be0e'
+    elsif arch == 'x86_64'
+      sha256 '794fd4d7c2bef6db359ca57b99842b31e0e9db8080f1b322034d0153c7cdd958'
     end
   else
-    os_ver = '12'
-    version '8.1.0'
+    os_ver = '13'
+    version '8.2.0'
     if arch == 'x86_64'
-      sha256 'ba41de1897a09aed6da5e5afdef6b4a6622ff034bf919d3eb7fd7b21b086994d'
+      sha256 '7f4731352886120fdf1eb201f4c3b146224907d35f6cbf14145aff26413988cd'
     elsif arch == 'arm64'
-      sha256 'nil'
+      sha256 '2bd11dbb7b1ed570b197638abf0e102ade0d029bd41fcd7bb914cc7c0658d5ff'
     end
   end
 
