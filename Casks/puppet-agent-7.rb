034380da38d8bacc6ac797be8102afa39a353dbf
Michael Hashizume <michael.hashizume@puppet.com>
2024-03-05 11:53:21 -0800

    Add puppet-agent 7.29.1

--- Casks/puppet-agent-7.rb
+++ Casks/puppet-agent-7.rb
@@ -8,35 +8,35 @@ cask 'puppet-agent-7' do
   case MacOS.version
   when '11'
     os_ver = '11'
-    version '7.29.0'
+    version '7.29.1'
     if arch == 'arm64'
-      sha256 '10376f328567118b4e01b1450017d508dabdfbec9f4f3655409ffcd09bc92df4'
+      sha256 '1a7f2116fd1ec5adcc4a9883eb5b70208a0653af795815a62ccdde1bcc33a8c0'
     elsif arch == 'x86_64'
-      sha256 'a9e71f4e3eed898a0d9b5307ccce1eaffcead5347ee051123eecc5883719118f'
+      sha256 '70a472fa21c04d9a477aa269b9c359ff19734da0e390ba4ecfae3a485758ab47'
     end
   when '12'
     os_ver = '12'
-    version '7.29.0'
+    version '7.29.1'
     if arch == 'arm64'
-      sha256 'a99a0402ce16e74410b0cc3c39d12b74543b7c2c3f13418483b864f8d6f76b2b'
+      sha256 'a536045668d969e8155a3a78e2359719c0d09187e96416fa69d0810d92cb98e7'
     elsif arch == 'x86_64'
-      sha256 '7311f8597dd9bca3acc1bae3bc7858a645782dfe4fa76173e8517d428b2eef48'
+      sha256 '56f4662796100f85207d0d61e3fd874a20b595aae326d97f99f7161493109ee7'
     end
   when '13'
     os_ver = '13'
-    version '7.29.0'
+    version '7.29.1'
     if arch == 'arm64'
-      sha256 '56bff289a803421fc393c06669b0baa7fb416b4b5e4204a0d7ea5903587e75a0'
+      sha256 '3e5e28f608956447166b2173081153523a7c313536a8e51c445184a23dc1389c'
     elsif arch == 'x86_64'
-      sha256 'f2acc367815dafb252041c8818206c75e8f33e77a8099447bc353b87a4cfcd67'
+      sha256 '516eef7700795629ea26125e855bede8a4d45169be07f16e3a18155b0185bce7'
     end
   else
     os_ver = '14'
-    version '7.29.0'
+    version '7.29.1'
     if arch == 'x86_64'
-      sha256 'fe39ec95c24247baaf629c6e9eb2237e00a0627c469e6241d36a6014bf99b0c7'
+      sha256 '2ea853aea0b1658d1b89b86e3cbef81adee3873aa2b162c9dc1c475268617461'
     elsif arch == 'arm64'
-      sha256 'f2ca9b8cd3e24cd4fe4c09dd0a440e99001398310316189975b895a58f12d8bc'
+      sha256 '0113172edca88faffb205e7ecfc18e7980d763513fc1e96830948245d9bc1ed2'
     end
   end
 
